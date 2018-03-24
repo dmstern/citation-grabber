fd4d2de9d035f0221103742b93f2de58357dae66
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-03-24 17:12:56 +0100

    code cleanup

--- index.js
+++ index.js
@@ -4,39 +4,15 @@ const puppeteer = require("puppeteer");
 const CRED = require("./cred");
 const CONFIG = require("./config");
 
-// console.log(cred, config);
-async function run() {
+async function login() {
   try {
     const browser = await puppeteer.launch({
       headless: process.env.NODE_ENV !== "dev"
     });
     const page = await browser.newPage();
 
-    await page.goto(
-      `https://accounts.google.com/Login?hl=de&amp;continue=https://scholar.google.de/scholar%3Fscilib%3D${
-        CONFIG.labelId
-      }`
-    ); // https://scholar.google.de/scholar?scilib=${CONFIG.labelId}
+    await page.goto(`https://accounts.google.com/Login?hl=de&amp`);
 
-    //// trying to access login from non logged in scholar page:
-    // const loginButton = '#gs_hdr_drw_bot > a';
-    // await page.evaluate(() => {
-    //   const loginContainer = '#gs_hdr_drw';
-    //   const loginContainerBot = '#gs_hdr_drw_bot';
-    //   const loginButton = '#gs_hdr_drw_bot > a';
-    //   const $ = window.$; //otherwise the transpiler will rename it and won't work
-    //   $(loginContainerBot).css({
-    //     'visibility': 'visible',
-    //     'display': 'block'
-    //   });
-    //   $(loginContainer).css({
-    //     'transform': 'none'
-    //   });
-    // });
-
-    // await page.click(loginButton);
-
-    // direct login:
     const userNameSelector = "#identifierId";
     const nextButton = "#identifierNext";
 
@@ -46,10 +22,6 @@ async function run() {
 
     const passwordSelector = '#password input[type="password"]';
     const passwordNextButton = '#passwordNext';
-    // page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-    // page.waitForSelector(passwordSelector);
-    // const pw = await page.$(passwordSelector);
-    // console.log("passwortSelector:___", pw);
     await new Promise(resolve => setTimeout(resolve, 2000));
     await page.click(passwordSelector);
     await page.keyboard.type(CRED.password);
@@ -63,4 +35,4 @@ async function run() {
   }
 }
 
-run();
+login();
